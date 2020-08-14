a70bdf03eae6c2d9474494bee56a18b6261f8026
Holk Neo <liaohaoxiang520@gmail.com>
2020-08-14 14:07:07 +0800

    init static page

---
+++ _next/static/6BW-zdL2ZKnVL9BmEwA5z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-b7e467bfc429d6ad8996.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0f4d1c674e26bb2d70f8.js"],"/posts/[id]":["static\u002Fchunks\u002Fpages\u002Fposts\u002F[id]-e8c562dc286e7e4b271b.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/qtP6VDlccOYJ-JuhJJjlm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-343471ddc13d6edcb041.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0f4d1c674e26bb2d70f8.js"],"/posts/[id]":["static\u002Fchunks\u002Fpages\u002Fposts\u002F[id]-641ef7cedfb9baa0f530.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
